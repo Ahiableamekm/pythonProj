a504bf909632b258021a6b91a53245f7b18cab3f
Mawuli Kofi Ahiableame <ahiableamekm@gmail.com>
2024-01-04 00:30:27 +0000

    refactor coffe machine program to oop approach

---
+++ coffee machine OOP/oop_practice.py
@@ -0,0 +1,23 @@
+# from turtle import Turtle, Screen
+
+# tom = Turtle()
+# print(tom)
+# tom.shape('turtle')
+# tom.color('coral')
+
+# tom.forward(100)
+
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+
+# table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+# table.add_column('Type', ['Electric', 'Water', 'Fire'])
+# table.align = 'l'
+
+# print(table)
