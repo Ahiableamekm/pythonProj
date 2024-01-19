b981b87bf7b25d754c505624773f2bb5ff65e515
Mawuli Kofi Ahiableame <ahiableamekm@gmail.com>
2024-01-19 17:04:42 +0000

    handled exceptions in the nato phonetic program

--- NATO alphabet project/main.py
+++ NATO alphabet project/main.py
@@ -3,6 +3,15 @@ import pandas
 data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-user_word = input("Enter a word: ").upper()
-phonetic_list = [phonetic_dict[letter] for letter in user_word]
-print(phonetic_list)
+
+def genrate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [phonetic_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, Only letters in the alpabet please")
+        genrate_phonetic()
+    else:
+        print(phonetic_list)
+        
+genrate_phonetic()
