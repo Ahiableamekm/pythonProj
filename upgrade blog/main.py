31356a488cd8e9847592d9d6ff4bc611fccbea11
Mawuli Kofi Ahiableame <ahiableamekm@gmail.com>
2024-02-08 15:47:57 +0000

    added reponsive functionality to contact page form.

--- upgrade blog/main.py
+++ upgrade blog/main.py
@@ -1,8 +1,12 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+from dotenv import dotenv_values
 from post import Post
 import requests
+import smtplib
 
+cred = dotenv_values(".env")
+EMAIL = cred["EMAIL"]
+PASSWORD = cred["PASS"]
 
 app = Flask(__name__)
 url = "https://api.npoint.io/8dc5244d07e4f67f1686"
@@ -14,15 +18,32 @@ def home():
     return render_template("index.html", data=post_object)
 
 
-@app.route("/about.html")
+@app.route("/about")
 def about():
-    return render_template("about.html")
+    
+        return render_template("about.html")
 
-@app.route("/contact.html")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method =="POST":
+        username = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL,
+                to_addrs=EMAIL,
+                msg=f"Subject: New connection from {username} \n\nUsername: {username} \nEmail: {email} \nPhone number: {phone} \nMessage: {message}"
+            )
+        return render_template("contact.html", status="Sucessfully sent message")
+    else:
+        
+        return render_template("contact.html", status="Contact me")
 
-@app.route("/post.html/<int:post_num>")
+@app.route("/post/<int:post_num>")
 def read_more(post_num):
     requested_post = None
     for pst in post_object:
@@ -30,6 +51,21 @@ def read_more(post_num):
             requested_post = pst
 
     return render_template("post.html", post = requested_post)
+
+
+# @app.route("/entry-form", methods=["POST"])
+# def receive_data():
+#     if request.method =="POST":
+#         username = request.form["name"]
+#         email = request.form["email"]
+#         phone = request.form["phone"]
+#         message = request.form["message"]
+#         print(username)
+#         print(email)
+#         print(phone)
+#         print(message)
+
+#         return f"<h1>Successfully sent your message</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
