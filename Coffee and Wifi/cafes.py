2ceb16fcaaec946349c0868de15092ef89850810
Mawuli Kofi Ahiableame <ahiableamekm@gmail.com>
2024-02-09 02:26:01 +0000

    built coffee & Wifi webiste

---
+++ Coffee and Wifi/cafes.py
@@ -0,0 +1,10 @@
+class Cafe:
+
+    def __init__(self, cafe_name, location_url, open_time, close_time, coffee_rating, wifi_rating, power):
+        self.cafe = cafe_name
+        self.location = location_url
+        self.open = open_time
+        self.close = close_time
+        self.coffee = coffee_rating
+        self.wifi = wifi_rating
+        self.power = power
